49a3e68d0fda6fb6bced2b668732c5f4f8d2ff38
ZloyGremlinJE <rellepro@gmail.com>
2020-04-08 12:04:31 +0300

    add myscript for tests

--- Lesson_8_task_1/script.js
+++ Lesson_8_task_1/script.js
@@ -3,3 +3,22 @@ let money,
     time;
     money = prompt("Ваш бюджет ?", 0);
     time = prompt("Введите дату в формате YYYY-MM-DD: ","2020-01-01");
+
+let appData = {
+    budget : money,
+    timeData : time,
+    expenses : {},
+    optionalExpenses : {},
+    income : [],
+    saving : false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+    
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
+
+alert(appData.budget / 30);
